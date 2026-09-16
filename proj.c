7acd861d9f62ddf178398b555e36d81e69c600f6
pz579 <pz579123456@gmail.com>
2026-09-16 09:33:51 -0300

    Inciando o cadastramento de dados

--- proj.c
+++ proj.c
@@ -1,4 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+
 int main (void)
 {
+    char *l[];
+    int cap[];
+    char *dias[];
 
+    int qtdL;
+    int qtdD;
+
+    printf("Digite a quantidade de laboratorios (maximo 20)");
+    scanf("%d", &qtdL);
+
+
+    printf("Digite a quantidade de dias (minimo 30)");
+    scanf("%d", &qtdL);
 }
